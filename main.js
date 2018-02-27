d9c7f68eb561306e64016b564df62efd38fb66c6
David Damnjanovic <36664588+AbrakaDaba@users.noreply.github.com>
2018-02-27 15:58:14 +0100

    Update main.js

--- main.js
+++ main.js
@@ -14,70 +14,77 @@ pause.on('click', pauseSlider);
 // });
 
 document.getElementsByTagName("body")[0].onload = function() { //JS funkcioniše
-    playSlider()
+  playSlider()
+  console.log(play);
 };
 
 
-function movingRight() {         //Right Arrow Function
-    pauseSlider();
-    // after.off('click');
-    console.log(this);
-    // setTimeout(function() {  
-    toTheRight()
-    // after.on('click');
-    // console.log(this); 
-    // }, 3000);
+function movingRight() { //Right Arrow Function
+  pauseSlider();
+  after.off('click');
+  console.log(after);
+  toTheRight()
+  setTimeout(function() {
+
+  after.on('click', movingRight);
+  }, 3000);
 }
 
 
-function movingLeft() {          //Left Arrow Function
-    pauseSlider();
-    // before.off('click');
-    toTheLeft();
-    // let imgs = $('.imgs');
-    imgs.each(function(e) { // Just for testing changing classes...
+function movingLeft() { //Left Arrow Function
+  pauseSlider();
+  before.off('click');
+  toTheLeft();
+  setTimeout(function() {
+
+  before.on('click', movingLeft);
+  }, 3000);
+  imgs.each(function(e) { // Just for testing changing classes...
     console.log(imgs[e]);
-    });
+  });
 };
 
 
 function playSlider() {
-    plays(0);
+  plays(0);
 }
 
 function pauseSlider() {
-    plays(1);
+  plays(1);
 }
 
 
 function plays(x) {
-    // play.off('click');
-    if (x == 0) {
-        toTheRight();
-        myInterval = setInterval(function() {
-        toTheRight()
-        // document.querySelector(".play").on
-        // play.on('click');
-        }, 4000);
-    }
-    if (x == 1) {
-        clearInterval(myInterval);
-        // pause.off('click');
-        // play.on('click');
-    }
+  // pause.on('click', pauseSlider);
+  // play.on('click');
+  if (x == 0) {
+    play.off('click');
+    toTheRight();
+    myInterval = setInterval(function() {
+      toTheRight()
+      // document.querySelector(".play").on
+      // play.on('click');
+    }, 4000);
+  }
+  if (x == 1) {
+    clearInterval(myInterval);
+    // pause.off('click');
+    play.on('click', playSlider);
+    console.log(play);
+  }
 }
 
 
 function toTheRight() {
-    $('.img3').removeClass('img3').addClass('imga2');
-    $('.img1').removeClass('img1').addClass('img3');
-    $('.img2').removeClass('img2').addClass('img1')
-    $('.imga2').removeClass('imga2').addClass('img2');
+  $('.img3').removeClass('img3').addClass('imga2');
+  $('.img1').removeClass('img1').addClass('img3');
+  $('.img2').removeClass('img2').addClass('img1')
+  $('.imga2').removeClass('imga2').addClass('img2');
 }
 
 function toTheLeft() {
-    $('.img2').removeClass('img2').addClass('imga3')
-    $('.img1').removeClass('img1').addClass('img2');
-    $('.img3').removeClass('img3').addClass('img1');
-    $('.imga3').removeClass('imga3').addClass('img3');
+  $('.img2').removeClass('img2').addClass('imga3')
+  $('.img1').removeClass('img1').addClass('img2');
+  $('.img3').removeClass('img3').addClass('img1');
+  $('.imga3').removeClass('imga3').addClass('img3');
 }
